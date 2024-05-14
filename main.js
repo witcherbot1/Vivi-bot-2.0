c194adbbf3c3b28cf1c0aa6cc33a2071a6e90504
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-05-14 20:48:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -551,7 +551,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `[ ⏳ ] Uptime: ${uptime}`;
+  const bio = `[ 😪 ] وقت التشغيل: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
