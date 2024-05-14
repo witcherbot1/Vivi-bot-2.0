d6d194c41504e3b5f9a3362dd127ea0fbcf53c97
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-05-14 20:43:52 +0300

    Create fun_زواج.js

---
+++ plugins/fun_زواج.js
@@ -0,0 +1,25 @@
+
+
+const toM = (a) => '@' + a.split('@')[0];
+function handler(m, {groupMetadata}) {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.fun_formarpareja
+
+  const ps = groupMetadata.participants.map((v) => v.id);
+  const a = ps.getRandom();
+  let b;
+  do b = ps.getRandom();
+  while (b === a);
+  m.reply(`╮─◆────◈💍◈────◆─╭
+  *${toM(a)}, ${tradutor.texto1[0]} ${toM(b)}, ${tradutor.texto1[1]}
+╯─◆────◈💍◈────◆─╰`, null, {
+    mentions: [a, b],
+  });
+}
+handler.help = ['زواج'];
+handler.tags = ['main', 'fun'];
+handler.command = ['زواج', 'زواج'];
+handler.group = true;
+export default handler;
