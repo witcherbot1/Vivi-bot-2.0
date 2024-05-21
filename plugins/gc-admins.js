7585368b7a0669e9b58933d67a3fae61dfa1cc77
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-05-21 05:16:13 +0300

    Create gc-admins.js

---
+++ plugins/gc-admins.js
@@ -0,0 +1,31 @@
+
+
+const handler = async (m, {conn, participants, groupMetadata, args}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/ar.json`))
+  const tradutor = _translate.plugins.gc_admins
+
+  const pp = await conn.profilePictureUrl(m.chat, 'image').catch((_) => null) || './src/admins.jpg';
+  const groupAdmins = participants.filter((p) => p.admin);
+  const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
+  const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
+  const pesan = args.join` `;
+  const oi = `${tradutor.texto1[3]} ${pesan}`;
+  const text = `╮═━═⬣استدعاء المشرفين⬣═━═╭
+ال${oi}
+
+*${tradutor.texto1[1]}*
+${listAdmin}
+
+*[⚠️] استخدام هاذا الامر لا يكون الا لضرورة ، غير ذلك قد تأخذ انذار وقد يصل لطرد.*
+╯═━───━═⬣❈⬣═━───━═╰`.trim();
+  conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
+};
+handler.help = ['admins <texto>'];
+handler.tags = ['group'];
+// regex detect A word without case sensitive
+handler.customPrefix = /a|@/i;
+handler.command = /^(admins|@admins|dmins)$/i;
+handler.group = true;
+export default handler;
