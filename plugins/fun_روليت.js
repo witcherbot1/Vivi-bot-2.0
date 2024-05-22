b8d5dac867443d4a5470be83563276550386b00e
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-05-22 23:15:12 +0300

    Update fun_روليت.js

--- plugins/fun_روليت.js
+++ plugins/fun_روليت.js
@@ -38,7 +38,7 @@ hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
 hours = (hours < 10) ? "0" + hours : hours
 minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
-return hours + ' ساعة (s) ' + minutes + ' دقيقة (s) '}
+return hours + ' ساعات و ' + minutes + ' دقيقة '}
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
