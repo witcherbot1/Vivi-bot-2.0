771dd418474df8e3dd5185c304fd8bf0e92562ce
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-05-27 06:41:56 +0300

    Update econ_روليت.js

--- plugins/fun_روليت.js
+++ plugins/fun_روليت.js
@@ -8,18 +8,24 @@ const handler = async (m, {conn, text, isPrems}) => {
 
 if (!db.data.chats[m.chat].game) throw tradutor.texto1 
 const date = global.db.data.users[m.sender].juegos + 21600000; //21600000 = 6 hs 
-if (new Date - global.db.data.users[m.sender].juegos < 21600000) throw `${tradutor.texto2[0]} ${msToTime(date - new Date())} ${tradutor.texto2[1]} `
+if (new Date - global.db.data.users[m.sender].juegos < 21600000) throw `*❐┃انتظر لين يخلص الوقت┃🤌❯*
+
+*▢ الوقت المتبقي :* ${msToTime(date - new Date())}`
 let user = global.db.data.users[m.sender]
 const prem = Math.floor(Math.random() * 3600000)
 const exp = Math.floor(Math.random() * 10000)
 const diamond = Math.floor(Math.random() * 300)
 const money = Math.floor(Math.random() * 10000)
-let rulet = ['text', 'text2', 'text3', 'text4', 'text5', 'text6']; 
-let ruleta = rulet[Math.floor(Math.random() * 6)]
+let rulet = ['text', 'text2', 'text5', 'text6']; 
+let ruleta = rulet[Math.floor(Math.random() * 4)]
 global.db.data.users[m.sender].juegos = new Date * 1;
+  
 if (ruleta === 'text') return m.reply(`${tradutor.texto3} ${exp} XP`).catch(global.db.data.users[m.sender].exp += exp) 
+  
 if (ruleta === 'text2') return m.reply(`${tradutor.texto4} ${exp} XP`).catch(global.db.data.users[m.sender].exp -= exp) 
+  
 if (ruleta === 'text5') return m.reply(`${tradutor.texto7} ${diamond} 💎`).catch(global.db.data.users[m.sender].diamond += diamond) 
+  
 if (ruleta === 'text6') return m.reply(tradutor.texto8).catch(global.db.data.users[m.sender].premium += prem) 
 }
 handler.help = ['game'];
@@ -42,4 +48,4 @@ return hours + ' ساعات و ' + minutes + ' دقيقة '}
 
 function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())];
-                           }
+}
