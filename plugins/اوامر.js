6b239bf344de566bc40746debaa44fb97fe661a3
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-10 03:47:33 +0300

    Create اوامر.js

---
+++ plugins/اوامر.js
@@ -0,0 +1,154 @@
+//Copyright ©JOANIMI/KILLUA
+//https://whatsapp.com/channel/0029Vab5oDNElagpHtJjmT0B
+
+import { prepareWAMessageMedia, generateWAMessageFromContent, getDevice } from '@whiskeysockets/baileys'
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+
+const handler = async (m, { conn, text, usedPrefix: prefijo }) => {
+    const device = await getDevice(m.key.id);
+    const mentionId = m.key.participant || m.key.remoteJid;
+
+    if (device !== 'desktop' || device !== 'web') {      
+        var joanimiimg = await prepareWAMessageMedia({ image: {url: 'https://telegra.ph/file/80c293df2f8f2514afa04.jpg'}}, { upload: conn.waUploadToServer })
+        const interactiveMessage = {
+            body: { text: `*▢ مرحبا بك يا : @${mentionId.split('@')[0]}*
+      ──━━━══⟐══━━━──
+ • # اختر القائمه من خلال الزر ☟
+ • # اكتب *" . "* قبل كل امر
+ • # مازال هذا البوت تحت التطوير اذا
+      واجهتك اي مشكله يرجى التواصل
+      مع المطور
+      ──━━━══⟐══━━━──
+▢ اسم البوت : *فـيـفـي - Vivi*
+▢ المطور : *بروك - 𝐾 ͟͟𝐿 ͟͟⁩*
+▢ مدة التشغيل : *${uptime}*
+╯─━━──═❆ ❮ ❖ ❯ ❆═──━━─╰`.trim() },
+            footer: { text: `*_˼‏ 𝙑 𝙞 𝙫 𝙞  𝘽 𝙤 𝙩 – v2.0 – 𝘽 𝙧 𝙤 𝙤 𝙠 ˹_*`.trim() },  
+            header: {
+                title: `╮━─═❆❮ قـائـمـة الاوامــر ❯❆═─━╭`,
+                subtitle: `╯─━━──═❆ ❮ ❖ ❯ ❆═──━━─╰`,
+                hasMediaAttachment: true,
+                imageMessage: joanimiimg.imageMessage,
+            },
+            nativeFlowMessage: {
+  						buttons: [
+  							{
+  								name: 'single_select',
+  						  	buttonParamsJson: JSON.stringify({
+  						  		title: 'اضـغـط هـنـا ➥',
+  						  		sections: [
+  						  			{
+  						  				title: '⟐═━─⏣─━═❪ 🇻 🇮 🇻 🇮   🇧 🇴 🇹 ❫═━─⏣─━═⟐',
+  							  	    highlight_label: '☚',
+  						  		    rows: [
+                                        {
+                                            header: '# قـسـم الـمـهـام🗂️ ⍅',
+                                            title: '.مهام',
+                                            description: '',
+                                            id: '.مهام'
+                                          }
+  						  		    ]
+  						  			},
+  						  			{
+  						  				highlight_label: '',
+  						  				rows: [
+  						  					{
+  						  		    		header: '# قـسـم الـطـلـبـات📥 ⍅',
+                            title: '.طلبات',
+                            description: '',
+                            id: '.طلبات'
+  						  		    	}
+  						  				]
+  						  			},
+  						  			{
+  						  				highlight_label: '',
+  						  				rows: [
+  						  					{
+  						  		    		header: '# قـسـم الـتـرفـيـه🎮 ⍅',
+                            title: '.ترفيه',
+                            description: '',
+                            id: '.ترفيه'
+  						  		    	}
+  						  				]
+  						  			},
+                                    	{
+  						  				highlight_label: '',
+  						  				rows: [
+  						  					{
+  						  		    		header: '# قـسـم الـديـن📿 ⍅',
+                            title: '.دين',
+                            description: '',
+                            id: '.دين'
+  						  		    	}
+  						  				]
+  						  			},
+                                    {
+                                        highlight_label: '',
+                                        rows: [
+                                            {
+                                            header: '# قـسـم الـمـطـور⚙️ ⍅',
+                            title: '.مطور',
+                            description: '',
+                            id: '.مطور'
+                                        }
+                                        ]
+                                    }
+  						  		]
+  						  	})
+  							},
+                              {
+                                  
+                              },
+                              {
+                                  
+                              },
+                              {
+                                  
+                              },
+                              {
+
+                                  
+                              },
+                              {
+                                  
+                              },
+                              {
+                                  
+                              },
+                              {
+                                  
+                              },
+                              {
+                                  
+                              }
+  			  		],
+                messageParamsJson: ''
+            }
+        };        
+
+        let msg = generateWAMessageFromContent(m.chat, {
+            viewOnceMessage: {
+                message: {
+                    interactiveMessage,
+                },
+            },
+        }, { userJid: conn.user.jid, quoted: m })
+        msg.message.viewOnceMessage.message.interactiveMessage.contextInfo = { mentionedJid: [mentionId] };
+        conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id });
+
+    } else {
+        conn.sendFile(m.chat, 'JoAnimi•Error.jpg', m);      
+    }    
+};
+handler.help = ['imgboton'];
+handler.tags = ['For Test'];
+handler.command = /^(تت)$/i;
+
+function clockString(ms) {
+    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+
+export default handler;
