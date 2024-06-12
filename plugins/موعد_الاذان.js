c523e1a5e4521ee9c783574ac2522d968aedca3e
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-12 07:02:34 +0300

    Update موعد_الاذان.js

--- plugins/موعد_الاذان.js
+++ plugins/موعد_الاذان.js
@@ -6,21 +6,29 @@ let handler = async (m, {
     usedPrefix,
     command
 }) => {
-if (!text) throw `مثال : \n ${usedPrefix + command} Kuwait`
+if (!text) throw `*❐┃صيغة غير صالحة┃❗❯*
+ *▢ مثال لاستخدام الامر :*
+ ◄ .أذان Kuwait
+
+ *▢ ملاحظة :* 
+يشترط ان يكون اسم الدولة باللغة الإنجليزية`
     try {
             let res = await fetchPrayerTimes(text)
-            m.reply(`${Object.entries(res).map(([name, data]) => `موعد *${name}* : ${data}`).join( \n ).trim()}`.trim())
+            m.reply(`╮═━─━╞مواقيت الاذان╡━─━═╭
+${Object.entries(res).map(([name, data]) => `▢ مـوعـد صـلاة *${name}* : ${data}`).join('\n').trim()}
+╯•═━──━═⪨•⚶•⪩═━──━═•╰
+> ملاحظة : هذه الاوقات قد تختلف من محافظة الى محافظة بفارق يسير فلا تعتمد عليها`.trim())
         } catch (e) {
             m.reply(eror)
         }
 }
-handler.help = [ adhan ]
-handler.tags = [ islam ]
-handler.command = /^(أذان)$/i
+handler.help = ['adhan']
+handler.tags = ['islam']
+handler.command = /^(أذان|اذان)$/i
 export default handler
 
 async function fetchPrayerTimes(q) {
-  const url =  https://athantime.me/  + q; // Ganti URL_HALAMAN_ADZAN dengan URL halaman web yang berisi informasi jadwal waktu adzan
+  const url = 'https://athantime.me/' + q; // Ganti URL_HALAMAN_ADZAN dengan URL halaman web yang berisi informasi jadwal waktu adzan
 
   try {
     const response = await fetch(url);
@@ -28,17 +36,17 @@ async function fetchPrayerTimes(q) {
     const $ = cheerio.load(html);
 
     const prayerTimes = {
-      اليوم: $( b:contains("الإثنين") ).text().trim(), // Mengambil informasi tanggal adzan saat ini
-      الفجر: $( td:contains("موعد اذان الفجر") ).next().text().trim(), // Mengambil waktu adzan Fajr
-      الظهر: $( td:contains("موعد اذان الظهر") ).next().text().trim(), // Mengambil waktu adzan Dhuhr
-      العصر: $( td:contains("موعد اذان العصر") ).next().text().trim(), // Mengambil waktu adzan Asr
-      المغرب: $( td:contains("موعد اذان المغرب") ).next().text().trim(), // Mengambil waktu adzan Maghrib
-      العشاء: $( td:contains("موعد اذان العشاء") ).next().text().trim(), // Mengambil waktu adzan Isha
-      الامساك: $( div.imsak li:contains("موعد الامساك اليوم") ).text().trim().split(  )[3], // Mengambil waktu imsak
-      };
+      
+      الفجر: $('td:contains("موعد اذان الفجر")').next().text().trim(), // Mengambil waktu adzan Fajr
+      الظهر: $('td:contains("موعد اذان الظهر")').next().text().trim(), // Mengambil waktu adzan Dhuhr
+      العصر: $('td:contains("موعد اذان العصر")').next().text().trim(), // Mengambil waktu adzan Asr
+      المغرب: $('td:contains("موعد اذان المغرب")').next().text().trim(), // Mengambil waktu adzan Maghrib
+      العشاء: $('td:contains("موعد اذان العشاء")').next().text().trim(), // Mengambil waktu adzan Isha
+      
+    };
     return prayerTimes;
   } catch (error) {
-    console.error( Terjadi kesalahan: , error);
+    console.error('Terjadi kesalahan:', error);
     return null;
   }
 }
