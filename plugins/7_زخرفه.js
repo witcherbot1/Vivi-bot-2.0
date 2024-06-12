8083f5cef107db784034fb02b7f9ba22cac3942c
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-12 17:32:26 +0300

    Update 7_زخرفه.js

--- plugins/7_زخرفه.js
+++ plugins/7_زخرفه.js
@@ -33,5 +33,5 @@ let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
 }
 handler.help = ['vivi']
 handler.tags = ['vivi']
-handler.command = ['زخرفة7', 'زخرفه7']
+handler.command = ['زخرفه7']
 export default handler
