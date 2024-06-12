6cc78dc8bdf4614e12dd27cb459fb13d4b341a87
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-12 05:05:25 +0300

    Update اصلاح.js

--- plugins/اصلاح2.js
+++ plugins/اصلاح2.js
@@ -39,6 +39,6 @@ const handler = async (m, { conn, usedPrefix }) => {
 };
 handler.help = ['del_reg_in_session_owner'];
 handler.tags = ['owner'];
-handler.command = /^(اصلاح2)$/i;
+handler.command = /^(اصلاح)$/i;
 handler.rowner = true
 export default handler;
