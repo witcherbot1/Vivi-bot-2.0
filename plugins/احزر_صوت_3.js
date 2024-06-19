42bad22c790bf60181c156e1a046b9580e1a1295
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-19 03:02:00 +0300

    Update احزر_صوت_3.js

--- plugins/احزر_صوت_3.js
+++ plugins/احزر_صوت_3.js
@@ -6,8 +6,8 @@ const handler = async (m, {conn}) => {
   if (!(id in conn.tebaklagu)) throw false;
   const json = conn.tebaklagu[id][1];
   const nya = json.jawaban;
-  const nyanya = nya.replace(/[ﻱﻮﻬﻨﻤﻠﻜﻘﻔﻐﻌﻈﻄﻀﺼﺸﺳﺯﺭﺫﺪﺨﺤﺠﺜﺘﺑﺍ]/g, '_');
+  const nyanya = nya.replace(/[ﻱ ﻭ ﻩ ﻥ ﻡ ﻝ ﻙ ﻕ ﻑ ﻍ ﻉ ﻅ ﻁ ﺽ ﺹ ﺵ ﺱ ﺯ ﺭ ﺫ ﺩ ﺥ ﺡ ﺝ ﺙ ﺕ ﺏ ﺍ]/g, '_');
   m.reply('' + nyanya + '');
 };
-handler.command = /^تلميح|غششيني$/i;
+handler.command = /^تلميح|غش$/i;
 export default handler;
