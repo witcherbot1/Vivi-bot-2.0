67618dee9368c39bec964ab90bb6834ec3525d4e
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-24 13:29:13 +0300

    Update احزر_صورة_2.js

--- plugins/احزر_صورة_2.js
+++ plugins/احزر_صورة_2.js
@@ -4,23 +4,23 @@ export async function before(m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم/i.test(m.quoted.text) || /.hhint/i.test(m.text))
         return !0
-    this.Vivianime = this.Vivianime ? this.Vivianime : {}
-    if (!(id in this.Vivianime))
+    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
+    if (!(id in this.tebakbendera))
         return this.reply(m.chat, '*❐┃ انـتـهـت الـلـعـبـة ┃❗ ❯*', m)
-    if (m.quoted.id == this.Vivianime[id][0].id) {
+    if (m.quoted.id == this.tebakbendera[id][0].id) {
         let isSurrender = /^(استسلم|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.Vivianime[id][3])
-            delete this.Vivianime[id]
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
             return this.reply(m.chat, '*❐┃طلع فاشل واستسلم┃🤭❯*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.Vivianime[id][1]))
+        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
 
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.Vivianime[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.Vivianime[id][2]} EXP*`, m)
-            clearTimeout(this.Vivianime[id][3])
-            delete this.Vivianime[id]
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.tebakbendera[id][2]} EXP*`, m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*❐┃قـريـب جـدااا┃🫣❯*`)
         else
