06f99387770dec6e8ac66a98b85a60e370fbbc63
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-25 20:07:02 +0300

    Update احزر_عيون2.js

--- plugins/احزر_عيون2.js
+++ plugins/احزر_عيون2.js
@@ -2,25 +2,25 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم/i.test(m.quoted.text) || /.hhint/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم|مدري/i.test(m.quoted.text) || /.hhint/i.test(m.text))
         return !0
-    this.Vivieyes = this.Vivieyes ? this.Vivieyes : {}
-    if (!(id in this.Vivieyes))
+    this.vivieyes = this.vivieyes ? this.vivieyes : {}
+    if (!(id in this.vivieyes))
         return this.reply(m.chat, '*❐┃ انـتـهـت الـلـعـبـة ┃❗ ❯*', m)
-    if (m.quoted.id == this.Vivieyes[id][0].id) {
-        let isSurrender = /^(استسلم|surr?ender)$/i.test(m.text)
+    if (m.quoted.id == this.vivieyes[id][0].id) {
+        let isSurrender = /^(استسلم|مدري)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.Vivieyes[id][3])
-            delete this.Vivieyes[id]
+            clearTimeout(this.vivieyes[id][3])
+            delete this.vivieyes[id]
             return this.reply(m.chat, '*❐┃طلع فاشل واستسلم┃🤭❯*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.Vivieyes[id][1]))
+        let json = JSON.parse(JSON.stringify(this.vivieyes[id][1]))
 
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.Vivieyes[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.Vivieyes[id][2]} EXP*`, m)
-            clearTimeout(this.Vivieyes[id][3])
-            delete this.Vivieyes[id]
+            global.db.data.users[m.sender].exp += this.vivieyes[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.vivieyes[id][2]} EXP*`, m)
+            clearTimeout(this.vivieyes[id][3])
+            delete this.vivieyes[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*❐┃قـريـب جـدااا┃🫣❯*`)
         else
