babf660016cccca623d616b543d378f99eb101cc
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-06-25 22:58:17 +0300

    Update احزر_صورة_2.js

--- plugins/احزر_صورة_2.js
+++ plugins/احزر_صورة_2.js
@@ -2,25 +2,23 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم/i.test(m.quoted.text) || /.hhint/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم|مدري/i.test(m.quoted.text) || /.hhint/i.test(m.text))
         return !0
-    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
-    if (!(id in this.tebakbendera))
-        return this.reply(m.chat, '*❐┃ انـتـهـت الـلـعـبـة ┃❗ ❯*', m)
-    if (m.quoted.id == this.tebakbendera[id][0].id) {
-        let isSurrender = /^(استسلم|surr?ender)$/i.test(m.text)
+    this.vivianime = this.vivianime ? this.vivianime : {}
+    if (m.quoted.id == this.vivianime[id][0].id) {
+        let isSurrender = /^(استسلم|مدري)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
+            clearTimeout(this.vivianime[id][3])
+            delete this.vivianime[id]
             return this.reply(m.chat, '*❐┃طلع فاشل واستسلم┃🤭❯*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
+        let json = JSON.parse(JSON.stringify(this.vivianime[id][1]))
 
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.tebakbendera[id][2]} EXP*`, m)
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
+            global.db.data.users[m.sender].exp += this.vivianime[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.vivianime[id][2]} EXP*`, m)
+            clearTimeout(this.vivianime[id][3])
+            delete this.vivianime[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*❐┃قـريـب جـدااا┃🫣❯*`)
         else
