bbaaa4e0730e46e9d82ce45928e2efa7266b383c
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-08 02:24:24 +0300

    Update احزر_عيون2.js

--- plugins/احزر_عيون2.js
+++ plugins/احزر_عيون2.js
@@ -2,27 +2,29 @@ import similarity from 'similarity'
 const threshold = 0.72
 export async function before(m) {
     let id = m.chat
-    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/.استسلم|مدري/i.test(m.quoted.text) || /.hhint/i.test(m.text))
+    if (!m.quoted || !m.quoted.fromMe || !m.text || !/اكتب :.*انسحب/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
         return !0
-    this.vivieyes = this.vivieyes ? this.vivieyes : {}
-    if (m.quoted.id == this.vivieyes[id][0].id) {
-        let isSurrender = /^(استسلم|مدري)$/i.test(m.text)
+    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
+    if (!(id in this.tebakbendera))
+        return this.reply(m.chat, '*❐┃السؤال انتهى شوفلك غيره┃🍋‍🟩❯*', m)
+    if (m.quoted.id == this.tebakbendera[id][0].id) {
+        let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.vivieyes[id][3])
-            delete this.vivieyes[id]
-            return this.reply(m.chat, '*❐┃طلع فاشل واستسلم┃🤭❯*', m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
+            return this.reply(m.chat, `*❐┃طلع فاشل واستسلم┃🤭❯*`, m)
         }
-        let json = JSON.parse(JSON.stringify(this.vivieyes[id][1]))
+        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
 
         if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.vivieyes[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.vivieyes[id][2]} EXP*`, m)
-            clearTimeout(this.vivieyes[id][3])
-            delete this.vivieyes[id]
+            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐┇الـجـائـزة💰↞${this.tebakbendera[id][2]} EXP*`, m)
+            clearTimeout(this.tebakbendera[id][3])
+            delete this.tebakbendera[id]
         } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
             m.reply(`*❐┃قـريـب جـدااا┃🫣❯*`)
         else
-            this.reply(m.chat, `❐┃اجـابـة خـاطـئـة┃❌ ❯`, m)
+            this.reply(m.chat, `*❐┃اجـابـة خـاطـئـة┃❌ ❯*`, m)
     }
     return !0
 }
