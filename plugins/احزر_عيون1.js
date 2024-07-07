c4f4070e1dc451c1df421a349572a87995f564bf
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-08 02:25:09 +0300

    Update احزر_عيون1.js

--- plugins/احزر_عيون1.js
+++ plugins/احزر_عيون1.js
@@ -1,31 +1,31 @@
 let timeout = 60000
 let poin = 1000
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.vivieyes = conn.vivieyes ? conn.vivieyes : {}
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (id in conn.vivieyes) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة على السؤال بعد┃❌ ❯', conn.vivieyes[id][0])
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '*❐╏لم يتم الاجابة علي السؤال بعد┃❌ ❯*', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://raw.githubusercontent.com/Brook-88/Game/main/game-eyes.json')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `╮─┈〈 *تخمين الشخصية👤*〉┈─⟐
 *▢❯* الوقت : *${(timeout / 1000).toFixed(2)}* ثانيه
-*▢❯* اكتب : *"استسلم"* للانسحاب
+*▢❯* اكتب : انسحب للأنسحاب
 *▢❯* الجائزة : *${poin}* EXP
 *▢❯* الرد على هذه الرسالة مع الاجابة!
 ╯┈──┈┈─┈┈┈┈──┈┈─┈⟐`.trim()
-    conn.vivieyes[id] = [
+    conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.vivieyes[id]) conn.reply(m.chat, `❮ ⌛┇انتهى الوقت┇⌛❯\n\n❐┇الاجـابـة✅↞ ${json.name}┇`, conn.vivieyes[id][0])
-            delete conn.vivieyes[id]
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `*❮ ⌛┇انتهي الوقت┇⌛❯*\n*❐↞┇الاجـابـة✅↞ ${json.name}┇*`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
-handler.help = ['guessflag']
-handler.tags = ['game']
+handler.help = ['عيون']
+handler.tags = ['fun']
 handler.command = /^عيون/i
 
 export default handler
