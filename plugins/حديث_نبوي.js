d3c613cf9e54d6ed8e36909e86a1653dccc8141e
Vivi 2.0 <Vivi.bot2.0@gmail.com>
2024-07-19 00:02:14 +0300

    Update حديث_نبوي.js

--- plugins/حديث_نبوي.js
+++ plugins/حديث_نبوي.js
@@ -2,7 +2,7 @@
 
 const handler = async (m, { conn }) => {
   try {
-      const photoLink = "https://api.bk9.site/Islam/hadith";
+      const photoLink = "https://apii.bk9.site/Islam/hadith";
       const photoFileName = "photo.jpg"; 
       await conn.sendMessage(m.chat, { image: { url: photoLink }, fileName: photoFileName, mimetype: 'image/jpeg', caption: '*❀ أحــاديــث نــبــويــة*' }, { quoted: m });
   } catch (error) {
